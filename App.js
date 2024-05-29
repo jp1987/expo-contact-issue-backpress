242516cf16f0dcc0618c2934e6a7b0bed07b5755
Jesper Paardekooper <j.paardekooper@rinkel.nl>
2024-05-29 14:02:36 +0200

    add example

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import * as Contacts from 'expo-contacts';
+import {useState} from "react";
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(false);
+
+  const onPress = async () => {
+    const result = await Contacts.presentContactPickerAsync();
+    console.log(JSON.stringify(result, null, 2));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Request permission" onPress={() => {
+        Contacts.requestPermissionsAsync().then((res) => {
+          setHasPermission(res.granted);
+        });
+      }} />
+      {hasPermission && <Button onPress={onPress} title="Pick a contact" />}
     </View>
   );
 }
